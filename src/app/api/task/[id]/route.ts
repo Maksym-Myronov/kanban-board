69411395a74b3e9055aa8152f313447dd6ebeee4
Maksym-Myronov <maksym.myronov2018@gmail.com>
2025-06-11 21:08:55 +0200

    fix: fix route file for new version next.js

--- src/app/api/task/[id]/route.ts
+++ src/app/api/task/[id]/route.ts
@@ -1,14 +1,14 @@
-import { NextRequest, NextResponse } from 'next/server';
 import { updateTaskStatus } from '@/core/data/index';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function PATCH(req: NextRequest, { params }: { params: { id: string } }) {
-    const { id } = params;
-    const { status } = await req.json();
+export async function PATCH(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
+    const { status } = await request.json();
 
     try {
         const updatedTask = await updateTaskStatus(id, status);
         return NextResponse.json({ updatedTask }, { status: 200 });
     } catch (error) {
-        return NextResponse.json({ error }, { status: 500 });
+        return NextResponse.json({ error: (error as Error).message }, { status: 500 });
     }
 }
